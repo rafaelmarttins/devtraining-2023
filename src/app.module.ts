b5c6c48eef56e001047bb6cc49335ddb79a507b4
rafaelmarttins <rafaelgemelli1@hotmail.com>
2023-10-27 17:19:16 -0400

    made improvements

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoursesController } from './courses/courses.controller';
-import { CoursesService } from './courses/courses.service';
+import { CoursesController } from './courses/controllers/courses.controller';
+import { CoursesService } from './courses/services/courses.service';
 
 @Module({
   imports: [],
