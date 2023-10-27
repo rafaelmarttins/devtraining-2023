676aeef89b8b2d7a5373e86bbe5163a952d46105
Rafael <rafaelgemelli1@hotmail.com>
2023-10-27 14:27:36 -0400

    :tada: Commit inicial

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly courseService: CoursesService) {}
+
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Listagem de cursos' });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso com ID ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+    return `Update course with ID ${id}`;
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Update course with ID ${id}`;
+  }
+}
