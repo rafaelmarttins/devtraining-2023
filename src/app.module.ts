ac7a1bdc7598818387f4413525969e213e4c5fb7
rafaelmarttins <rafaelgemelli1@hotmail.com>
2023-11-10 10:58:44 -0400

    Update project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoursesController } from './courses/controllers/courses.controller';
-import { CoursesService } from './courses/services/courses.service';
 import { DatabaseModule } from './database/database.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController, CoursesController],
-  providers: [AppService, CoursesService],
+  imports: [CoursesModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
